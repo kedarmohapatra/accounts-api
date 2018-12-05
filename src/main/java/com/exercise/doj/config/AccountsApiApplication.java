8a0d034f4fae839719e851e14262f661fd231072
kedarmohapatra <kedar.mohapatra@gmail.com>
2018-12-05 12:09:57 +0000

    Initial commit

---
+++ src/main/java/com/exercise/doj/config/AccountsApiApplication.java
@@ -0,0 +1,12 @@
+package com.exercise.doj.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApiApplication.class, args);
+	}
+}
---
+++ src/main/java/com/exercise/doj/model/User.java
@@ -0,0 +1,41 @@
+package com.exercise.doj.model;
+
+public class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String accountNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+}
